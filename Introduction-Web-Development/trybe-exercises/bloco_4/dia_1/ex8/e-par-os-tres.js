a787c054899e95ea56eba2f68459533ce70cc65c
gabrielgarciamendonca <gabriel.dev.lop@gmail.com>
2020-11-25 19:28:56 -0300

    Adicionar exercicio 9 e corrigir o 8

--- Introduction-Web-Development/trybe-exercises/bloco_4/dia_1/ex8/e-par-os-tres.js
+++ Introduction-Web-Development/trybe-exercises/bloco_4/dia_1/ex8/e-par-os-tres.js
@@ -1,5 +1,5 @@
 let num1 = 4;
-let num2 = 2;
-let num3 = 2;
+let num2 = 3;
+let num3 = 3;
 
-console.log(num1%3 == 0 || num2%3 ==0 || num3%3 ==0)
+console.log(num1%2 == 0 || num2%2 ==0 || num3%2 ==0)
