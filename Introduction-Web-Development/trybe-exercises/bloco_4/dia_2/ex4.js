468b0c8fef8d65f4207ee66e8bed5aacf9bae9eb
gabrielgarciamendonca <gabriel.dev.lop@gmail.com>
2020-11-26 17:49:20 -0300

    Adicionar ex4

---
+++ Introduction-Web-Development/trybe-exercises/bloco_4/dia_2/ex4.js
@@ -0,0 +1,16 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let qtde = numbers.length;
+let sum = 0;
+
+for (let index = 0; index < qtde; index += 1) {
+    sum += numbers[index];
+}
+
+let average = sum / qtde;
+
+if(average > 20){
+    console.log('Valor maior que 20');
+}else{
+    console.log('Valor menor ou igual a 20');
+}
