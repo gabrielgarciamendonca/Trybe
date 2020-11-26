6cb4a5f79a49546fcd1081fa2a131daf8b28125b
gabrielgarciamendonca <gabriel.dev.lop@gmail.com>
2020-11-26 18:05:36 -0300

    Adicionar ex7

---
+++ Introduction-Web-Development/trybe-exercises/bloco_4/dia_2/ex7.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let smaller = 0;
+
+for(let index = 0; index < numbers.length; index += 1){
+    if(index == 0){
+        smaller = numbers[index];
+    }
+    if(numbers[index] < smaller){
+        smaller = numbers[index];
+    }
+}
+
+console.log('O menor valor é: ', smaller);
