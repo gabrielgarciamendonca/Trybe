a31e230294f3055800646f419db54253fa47f616
gabrielgarciamendonca <gabriel.dev.lop@gmail.com>
2020-11-26 20:15:39 -0300

    Adicionar exercicios-bonus-2

--- Introduction-Web-Development/trybe-exercises/bloco_4/dia_2/bonus/ex1-bonus.js
+++ Introduction-Web-Development/trybe-exercises/bloco_4/dia_2/bonus/ex1-bonus.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for(let index = 1; index < numbers.length; index += 1){
     for(let position = 0; position < index; position += 1){
-        if(numbers[index] < numbers[position]){
+        if(numbers[index] > numbers[position]){
             let oldPositon = numbers[index];
             numbers[index] = numbers[position];
             numbers[position] = oldPositon;
