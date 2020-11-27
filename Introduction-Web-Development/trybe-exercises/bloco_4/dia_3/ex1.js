f554ce748ce2c7ba12137324404dc2770abec906
gabrielgarciamendonca <gabriel.dev.lop@gmail.com>
2020-11-27 18:01:06 -0300

    Adicionar ex2

--- Introduction-Web-Development/trybe-exercises/bloco_4/dia_3/ex1.js
+++ Introduction-Web-Development/trybe-exercises/bloco_4/dia_3/ex1.js
@@ -2,7 +2,7 @@ let n = 8;
 
 for(let index = 0; index < n; index += 1){
     let chars = '';
-    for (let index = 0; index < n; index += 1){
+    for (let index1 = 0; index1 < n; index1 += 1){
         chars += '*';
     }
     console.log(chars);
