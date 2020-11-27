ebca2de860ee7866406d83963aae3b25c61ba91d
gabrielgarciamendonca <gabriel.dev.lop@gmail.com>
2020-11-27 17:50:39 -0300

    Adicionar ex1

---
+++ Introduction-Web-Development/trybe-exercises/bloco_4/dia_3/ex1.js
@@ -0,0 +1,9 @@
+let n = 8;
+
+for(let index = 0; index < n; index += 1){
+    let chars = '';
+    for (let index = 0; index < n; index += 1){
+        chars += '*';
+    }
+    console.log(chars);
+}
