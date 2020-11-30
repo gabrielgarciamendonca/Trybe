5efd0dd4e4a50e1939c7d4ef0cdbe1960c3219bc
gabrielgarciamendonca <gabriel.dev.lop@gmail.com>
2020-11-30 19:02:44 -0300

    Adicionar ex2

--- Introduction-Web-Development/trybe-exercises/bloco_4/dia_4/parte_2 - funcoes/functions.js
+++ Introduction-Web-Development/trybe-exercises/bloco_4/dia_4/parte_2 - funcoes/functions.js
@@ -13,7 +13,68 @@ function verificaPalindrome(text){
   }
 }
 
+function lowestIndex(arr){
+  let menor = 0;
+  let indexLower = 0;
+  for (let index in arr) {
+    if (index === 0) {
+      menor = arr[index];
+      indexLower = index;
+    } else if (arr[index] < menor) {
+      menor = arr[index];
+      indexLower = index;
+    }
+  }
+  return indexLower;
+}
+
+function higherIndex(arr){
+  let maior = 0;
+  let indexGreat = 0;
+  for (let index in arr) {
+    if(index === 0){
+      maior = arr[index];
+      indexGreat = index;
+    }else if(arr[index] > maior){
+      maior = arr[index];
+      indexGreat = index;
+    }
+  }
+  return indexGreat;
+}
+
+function biggestCharacter(names){
+  let biggest = 0;
+  let name;
+  for (let index in names){
+    if(index === 0){
+      biggest = names[index].length;
+      name = names[index];
+    }else if(biggest < names[index].length){
+      biggest = names[index].length;
+      name = names[index];
+    }
+  }
+  return name;
+}
+
+function moreRepeated(numbers){
+  let repeat={
+    number: 0,
+    repeated: 0,
+  };
+  for(let index in numbers){
+    for(let pos in numbers){
+      if(numbers[index] === numbers[pos]){
+        repeat += 1;
+      }
+    }
+  }
+}
 
 //hub
 console.log(verificaPalindrome('luzazul'));
+console.log(higherIndex([2, 3, 6, 7, 10, 1]));
+console.log(lowestIndex([2, 4, 6, 7, 10, 0, -3]));
+console.log(biggestCharacter(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 
